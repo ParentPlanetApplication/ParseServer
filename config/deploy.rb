5aeca0953543b25ca7bdb256357a8c707932b3a0
Ngo Hoai Phuong <phuongnh@vinasource.com>
2016-08-12 12:43:31 +0700

    add task

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,13 @@ set :ssh_options, { :forward_agent => true }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    invoke 'pm2:restart'
+  end
+
+  after :publishing, :restart
+end
